c0ca371eca5961e871693240dba3cb6a47b1e556
xfnw <xfnw@riseup.net>
2024-11-18 23:51:10 -0500

    parse arguments

--- solamon.py
+++ solamon.py
@@ -23,17 +23,30 @@ class Server(BaseServer):
         print(f"{self.name} > {line.format()}")
 
 class Bot(BaseBot):
-    def __init__(self, url):
+    def __init__(self, url, delay):
         super().__init__()
+        self.default_delay = delay
         self.url = url
 
-    def create_server(self, name, delay):
-        return Server(self, name, delay)
+    def create_server(self, name):
+        return Server(self, name, self.default_delay)
 
-def main():
+async def main():
     parser = argparse.ArgumentParser()
-
+    parser.add_argument("url")
+    parser.add_argument("-d", help="delay in minutes between collecting metrics", type=int, default=10)
+    parser.add_argument("-n", help="nickname to use", default="solamon")
+    parser.add_argument("-s", help="irc server(s) to connect to", action='append')
     args = parser.parse_args()
 
+    bot = Bot(args.url, args.d)
+
+    for server in args.s:
+        params = ConnectionParams(args.n, server, 6697)
+        await bot.add_server(server, params)
+
+    await bot.run()
+
+
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
