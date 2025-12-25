d9274e31ebe423069cdd95bbf3e54512afd732ba
xfnw <xfnw@riseup.net>
2025-12-24 23:59:06 -0500

    flake: override anyio

--- flake.nix
+++ flake.nix
@@ -8,9 +8,22 @@
       "x86_64-linux"
     ] (system: p nixpkgs.legacyPackages.${system});
   in {
-    packages = forAllSystems (pkgs: {
+    packages = forAllSystems (pkgs: let
+      pp = pkgs.python3Packages;
+      anyio = pp.anyio.overridePythonAttrs (old: rec {
+        version = "2.0.2";
+        doCheck = false; # annoyingly slow
+        src = pkgs.fetchFromGitHub {
+          owner = "agronholm";
+          repo = "anyio";
+          rev = "refs/tags/${version}";
+          hash = "sha256-u0/6hrsS/vGxfSK/oc3ou+O6EeXJ6nfpuJRpUbP7yho=";
+        };
+      });
+      ircrobots = pp.ircrobots.override { inherit anyio; };
+    in {
       default = pkgs.writers.writePython3Bin "solamon" {
-        libraries = with pkgs.python3Packages; [ aiohttp ircrobots ];
+        libraries = [ pp.aiohttp ircrobots ];
         flakeIgnore = [ "E265" "E401" "E501" "W503" ];
       } ./solamon.py;
     });
